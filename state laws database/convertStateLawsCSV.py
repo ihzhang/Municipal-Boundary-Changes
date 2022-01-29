429e1b61481fe545a72e31b94ccbf15e8421fb6e
Poojit Hegde <phegde8@stanford.edu>
2022-01-28 16:27:42 -0800

    New folder for additions to state laws database from Durst.
    Contains:
    - statelawsdatabase1.csv
    - statelawsdatabase2.csv
    - convertStateLawsCSV.py
    
    the first database is the original with 1's and 0's, the second contains a CSV list of laws per state, and the third is the script for reference.

---
+++ state laws database/convertStateLawsCSV.py
@@ -0,0 +1,33 @@
+
+import csv
+
+#this program converts the state laws database collecting the sourced data from Durs
+# the original database is in 1's and 0's for if the laws exist, the new database will list
+#   each state's categories for which there are laws
+
+original_file_name = "statelawsdatabase1.csv"
+new_file_name = "statelawsdatabase2.csv"
+header = []
+rows = []
+with open(original_file_name, 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+
+new_header = ["State", "Laws"]
+
+new_rows = []
+
+for row in rows:
+    new_row = [row[0]]
+    for elementnum in range(1,len(row)):
+        if row[elementnum] == "1":
+            new_row.append(header[elementnum])
+    new_rows.append(new_row)
+    print(new_row)
+
+with open(new_file_name, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(new_header)
+    csvwriter.writerows(new_rows)
